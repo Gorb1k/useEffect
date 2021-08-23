b100d30c8b9a6e75f4784b834c02fb413273c15a
Ihor Horb <gorbigorolegovich@gmail.com>
2021-08-23 18:26:52 +0300

    timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import './app.css'
 import axios from 'axios'
 
@@ -16,19 +16,110 @@ type UserType = {
     followers: number
 }
 
-export const GitHub = () => {
-    const [selectUser, setSelectUser] = useState<SearchUserType | null>(null)
-    const [users, setUsers] = useState<SearchUserType[]>([])
-    const [userDetail, setUserDetail] = useState<null|UserType>(null)
-    const [tempSearch, setTempSearch] = useState<string>('gorbik')
+type SearchPropsType = {
+    value: string
+    onSubmit: (fixedValue: string) => void
+}
+export const Search: FC<SearchPropsType> = ({onSubmit, value}) => {
 
-    const fetchData = (term:string) => {
+    const [tempSearch, setTempSearch] = useState<string>('')
+
+    useEffect(() => {
+        setTempSearch(value)
+    }, [value])
+    return (
+        <div>
+            <input value={tempSearch}
+                   onChange={(e) => {
+                       setTempSearch(e.currentTarget.value)
+                   }}
+                   type="text"
+                   placeholder={'search'}/>
+            <button onClick={() => {
+                onSubmit(tempSearch)
+            }}>
+                Find
+            </button>
+        </div>
+    )
+}
+type UsersListPropsType = {
+    term: string
+    selectUser: SearchUserType | null
+    onUserSelect: (user: SearchUserType) => void
+}
+export const UsersList: FC<UsersListPropsType> = ({selectUser, onUserSelect, term}) => {
+    const [users, setUsers] = useState<SearchUserType[]>([])
+    useEffect(() => {
         axios.get<SearchResult>(`https://api.github.com/search/users?q=${term}`)
             .then((res) =>
                 setUsers(res.data.items)
             )
-    }
 
+    }, [term])
+    return (
+        <ul className={'user-list'}>
+            {users.map((u) =>
+                <li key={u.id}
+                    className={selectUser?.id === u.id ? 'selected' : ''}
+                    onClick={() => {
+                        onUserSelect(u)
+                    }}>
+                    {u.login}
+                </li>)}
+        </ul>
+    )
+}
+type TimerProps = {
+
+}
+export const Timer:FC<TimerProps> = ({}) => {
+    const [seconds, setSeconds] = useState(60)
+    useEffect(() => {
+        setInterval(() => {
+            setSeconds((sec) => sec - 1)
+        },1000)
+    },[])
+  return(
+      <div>
+          {seconds}
+      </div>
+  )
+}
+type UserDetailsType = {
+    user: SearchUserType | null
+}
+export const UserDetails: FC<UserDetailsType> = ({user}) => {
+    const [userDetail, setUserDetail] = useState<null | UserType>(null)
+    useEffect(() => {
+        console.log('sync user details')
+        if (user) {
+            axios
+                .get<UserType>(`https://api.github.com/users/${user.login}`)
+                .then((res) =>
+                    setUserDetail(res.data)
+                )
+        }
+
+    }, [user])
+    return (
+        <div>
+            {userDetail && <div>
+                <h2>{userDetail.login}</h2>
+                <Timer/>
+                <img src={userDetail.avatar_url} alt=""/>
+                <br/>
+                {userDetail.login}
+                <br/>
+                followers: {userDetail.followers}
+            </div>}
+        </div>
+    )
+}
+
+export const GitHub = () => {
+    const [selectUser, setSelectUser] = useState<SearchUserType | null>(null)
+    const [searchTerm, setSearchTerm] = useState<string>('gorbik')
 
     useEffect(() => {
         console.log('sync')
@@ -36,60 +127,20 @@ export const GitHub = () => {
             document.title = selectUser.login + ' App'
         }
     }, [selectUser])
-    useEffect(() => {
-        fetchData(tempSearch)
-    }, [])
-    useEffect(() => {
-        console.log('sync user details')
-        if (selectUser) {
-            axios
-                .get<UserType>(`https://api.github.com/users/${selectUser.login}`)
-                .then((res) =>
-                    setUserDetail(res.data)
-                )
-        }
-
-    },[selectUser])
 
     return (
         <div className={'container'}>
             <div className={'sidebar'}>
-                <div>
-                    <input value={tempSearch}
-                           onChange={(e) => {
-                               setTempSearch(e.currentTarget.value)
-                           }}
-                           type="text"
-                           placeholder={'search'}/>
-                    <button onClick={() => {
-                        fetchData(tempSearch)
-                    }}>
-                        Find
-                    </button>
-                </div>
-                <ul className={'user-list'}>
-                    {users.map((u) =>
-                        <li key={u.id}
-                            className={selectUser?.id === u.id ? 'selected' : ''}
-                            onClick={() => {
-                                setSelectUser(u)
-                            }}>
-                            {u.login}
-                        </li>)}
-                </ul>
+                <Search value={searchTerm} onSubmit={(fixedValue) => {
+                    setSearchTerm(fixedValue)
+                }}/>
+                <button onClick={() => {
+                    setSearchTerm('gorbik')
+                }}>Reset
+                </button>
+                <UsersList term={searchTerm} selectUser={selectUser} onUserSelect={setSelectUser}/>
             </div>
-            <div>
-                <h2>Username</h2>
-                {userDetail && <div>
-                    <img src={userDetail.avatar_url} alt=""/>
-                    <br/>
-                    {userDetail.login}
-                    <br/>
-                    followers: {userDetail.followers}
-                </div>}
-            </div>
+            <UserDetails user={selectUser}/>
         </div>
-
-
     )
 }
